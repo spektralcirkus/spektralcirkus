97ccbb091cd3f889f0bb6d51d86fc6d96fc4a457
lsrdg <eliasrodrigo@protonmail.com>
2016-06-02 23:21:08 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="malabarismo, línguas, cubo mágico" />
+    <title>Cirklo Cirko - malabaristicamente</title>
+
+    <link href="../css/responsive.min.css" rel="stylesheet" />
+    <link href="../css/lang.css" rel="stylesheet" />
+</head>
+<body>
+  <nav>
+    <div class="btn-group">
+      <a href="../"><button>Cirklo Cirko</button> </a>
+      <a href="malabares/"><button>malabaristicamente</button> </a>
+      <a href="linguagem/"><button>linguagem</button> </a>
+      <a href="magico-cubo/"><button>mágico cubo</button> </a>
+      <a href="photoblog/"><button>photoblog</button> </a>
+      <a href="entre-em-contato/"><button>entre em contato</button> </a>
+      <a href="sobre/"><button>sobre</button></a>
+    </div>
+  </nav>
+  
+  <header>
+    <img src="../img/headerimg.jpg" alt="claves de fogo" width="300" height="100" class="headerimg">
+    <h1>Cirklo Cirko</h1>
+    <h2>Malabarísticamente</h2>
+  </header>
+  <hr>
+  <main>
+  <div class="container">
+  
+   <div class="row">
+
+    <div class="col-xxs-6">
+    <section>
+      <h3>Claves e a estação - parte 4 - outono 2015</h3>
+      <p>a quantas andam as claves pelo outono</p>
+     </section>
+    </div>
+
+    <div class="col-xxs-6">
+      <section>
+      <h3>Claves e a estação - parte 3 - verão 2015</h3>
+      <p>sobre claves de... ops, no sol.
+      </section>
+    </div>
+  
+   </div>
+  <div class="row">
+ 
+    <div class="col-xxs-6">
+      <section>
+      <h3>Ensaio fotográfico com fogo</h3>
+      <p>um pouco do que rolou em Nørrebro</h3>
+      </section>
+    </div>
+  
+    <div class="col-xxs-6">
+     <section>
+      <h3>5 bolas - solidificando</h3>
+      <p>...chegou a hora de aprender 5 pra valer</p>
+     </section>
+    </div>
+   
+   </div>
+   </div>
+  </div>
+  </main>
+  <hr>
+  <div class="container">
+  <section>
+  <div class="row">
+  <div class="col-xxs-4">
+  <ul>
+  <li><h3>Malabarismo</h3></li>
+    <ul>
+    <li><p>Claves</p></li>
+    <li><p>Bolas</p></li>
+    <li><p>Contato</p></li>
+    </ul>
+  </ul>
+  </div>
+  <div class="col-xxs-4">
+    <ul>
+  <li><h3>Linguagem</h3></li>
+    <ul>
+    <li><p>terminologia</p></li>
+    <li><p>Vocabulário malabarístico - inglês<p></li>
+    </ul>
+  </ul>
+  </div>
+  <div class="col-xxs-4">
+    <ul>
+  <li><h3>Mágico cubo</h3></li>
+    <ul>
+    <li><p>Cubo mágico e malabares</p></li>
+    </ul>
+  </ul>
+  </div>
+  </div>
+  </section>
+  </div>
+  <hr>
+  <footer>
+  <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Licença Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Cirklo Cirko</span> de <a xmlns:cc="http://creativecommons.org/ns#" href="http://cirklocirko.synergize.co" property="cc:attributionName" rel="cc:attributionURL">Elias Rodrigo</a> está licenciado com uma Licença <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons - Atribuição-NãoComercial 4.0 Internacional</a>.<br />Baseado no trabalho disponível em <a xmlns:dct="http://purl.org/dc/terms/" href="http://cirklocirko.synergize.co" rel="dct:source">http://cirklocirko.synergize.co</a>.</footer>
+    <script src="../js/jquery-2.1.4.min.js"></script>
+    <script src="../jsresponsive.min.js"></script>
+</body>
+</html>
