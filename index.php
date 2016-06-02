3e4861c5237e158dde4854a0c1465e15c6548631
lsrdg <eliasrodrigo@protonmail.com>
2016-06-02 22:47:06 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Your site description." />
+    <title>Cirklo Cirko</title>
+
+    <link href="css/responsive.min.css" rel="stylesheet" />
+    <link href="css/custom.css" rel="stylesheet" />
+</head>
+<body>
+  <header>
+  <img src="img/headerimg.jpg" alt="fire juggling" hight="90" width="500" class="headerimg">
+  <h1>Cirklo Cirko</h1>
+  </header>
+  <hr>
+  <main>
+  <div class="container">
+    <div class="row">
+  <section>
+    <div class="col-xxs-6">
+      <a href="pt/"><button><h2>potuguês</h2></button></a>
+    </div>
+  </section>
+  <section>
+    <div class="col-xxs-6">
+     <button><h2>english</h2></button>
+    </div>
+  </section
+  </div>
+  <div class="row">
+  <section>
+    <div class="col-xxs-6">
+      <button><h2>esperanto</h2></button>
+    </div>
+  </section>
+  <section>
+    <div class="col-xxs-6">
+     <button><h2>español</h2></button>
+    </div>
+  </section
+  </div>
+  <div class="row">
+  <section>
+    <div class="col-xxs-6">
+    <button><h2>dansk</h2></button>
+    </div>
+  </section>
+  <section>
+    <div class="col-xxs-6">
+     <button><h2>photoblog</h2></button>
+    </div>
+  </section
+  </div>
+  </div>
+  </main>
+  <hr>
+  <footer>
+  <p> <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Creative Commons Licence" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Cirklo Cirko</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://cirklocirko.synergize.co" property="cc:attributionName" rel="cc:attributionURL">Elias Rodrigo</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.<br />Based on a work at <a xmlns:dct="http://purl.org/dc/terms/" href="http://cirklocirko.synergize.co" rel="dct:source">http://cirklocirko.synergize.co</a>.</p>
+  </footer>
+    <script src="js/jquery-2.1.4.min.js"></script>
+    <script src="js/responsive.min.js"></script>
+</body>
+</html>
